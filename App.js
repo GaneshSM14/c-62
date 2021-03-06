c8cc5902b959c79e1d6c0caa5efb88982156a426
GaneshSM14 <65331734+GaneshSM14@users.noreply.github.com>
2021-03-06 11:39:09 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import {Text, View, StyleSheet, Button }from 'react-native';
+export default class App extends React.Component{
+render(){
+  return(
+    <View>
+      <Button 
+      title="Project 1">
+      </Button>
+    </View>
+  )
+}
+
+}
